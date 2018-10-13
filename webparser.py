c3ffa5b655c6e6afa29a0f8333e638b6104eb5cc
porterwisnie <porterwisniewski@gmail.com>
2018-10-13 13:41:50 -0700

    created post finder and taking list input

--- webparser.py
+++ webparser.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 import requests
 from bs4 import BeautifulSoup as bs4
-#change this line to input() or a list of subs when done testing
-subreddit = 'learnpython'
+import time
+def post_finder(subreddit):
+    
 
-url = 'https://new.reddit.com/r/'+subreddit+'/top/?t=all'
+    url = 'https://new.reddit.com/r/'+subreddit+'/top/?t=all'
 
-headers = {'user-agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0'}
+    headers = {'user-agent':'crawler for programming resources from subreddits porterwisnie resourcesmaster'}
 
-r = requests.get(url,headers=headers)
+    r = requests.get(url,headers=headers)
 
-soup = bs4(r.text,'lxml')
+    soup = bs4(r.text,'lxml')
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+    all_outbound_links = [i.get('href') for i in soup.find_all('a')]
+
+    post_links = []
+
+    for link in all_outbound_links:
+        if 'comments' in link and 'http' in link:
+            post_links.append(link)
+    print(post_links)
+    time.sleep(2)
+
+subreddits_to_crawl = ['learnpython','java','Python']
+
+for sub in subreddits_to_crawl:
+    post_finder(sub)
