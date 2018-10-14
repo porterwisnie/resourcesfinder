22e8d52eb79df489003476899fdd35e6e09f0d29
Porter Wisniewski <porterwisniewski@gmail.com>
2018-10-13 17:37:27 -0700

    Update webparser.py

--- webparser.py
+++ webparser.py
@@ -3,11 +3,12 @@ import requests
 from bs4 import BeautifulSoup as bs4
 import time
 #next step is to find all a tags in each post link
+
+headers = {'user-agent':'crawler for programming resources from subreddits porterwisnie resourcesmaster'}
+
 def post_finder(subreddit):
 
     url = 'https://new.reddit.com/r/'+subreddit+'/top/?t=all'
-
-    headers = {'user-agent':'crawler for programming resources from subreddits porterwisnie resourcesmaster'}
 
     r = requests.get(url,headers=headers)
 
@@ -20,10 +21,24 @@ def post_finder(subreddit):
     for link in all_outbound_links:
         if 'comments' in link and 'http' in link:
             post_links.append(link)
-    print(post_links)
+    return post_links
     time.sleep(2)
 
+def comment_parser():
+
+	for link in post_links:
+
+		r = requests.get(link,headers=headers)
+
+		soup = bs4(r.text,'lxml')
+
+		links_in_posts = [i.get('href') for i in soup.find_all('a')]
+
+		print(links_in_posts)
+
+		time.sleep(2)
 subreddits_to_crawl = ['learnpython','java','Python']
 
 for sub in subreddits_to_crawl:
     post_finder(sub)
+
