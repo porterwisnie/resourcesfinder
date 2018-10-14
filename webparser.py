a504892033c1d57e35b37151c7a9b784c8940e03
Porter Wisniewski <porterwisniewski@gmail.com>
2018-10-13 21:12:49 -0700

    Update webparser.py

--- webparser.py
+++ webparser.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup as bs4
 import time
 #next step is to find all a tags in each post link
 
+post_links = []
+
 headers = {'user-agent':'crawler for programming resources from subreddits porterwisnie resourcesmaster'}
 
 def post_finder(subreddit):
@@ -16,29 +18,33 @@ def post_finder(subreddit):
 
     all_outbound_links = [i.get('href') for i in soup.find_all('a')]
 
-    post_links = []
 
     for link in all_outbound_links:
         if 'comments' in link and 'http' in link:
             post_links.append(link)
-    return post_links
+  
     time.sleep(2)
 
-def comment_parser():
+def comment_parser(post):
 
-	for link in post_links:
+	for link in post:
 
 		r = requests.get(link,headers=headers)
 
-		soup = bs4(r.text,'lxml')
+		soup2 = bs4(r.text,'lxml')
 
-		links_in_posts = [i.get('href') for i in soup.find_all('a')]
+		links_in_posts = []
+
+		for item in soup2.find_all('a'):
+			link = item.get('href')
+			if link[0:4] == 'http':
+				links_in_posts.append(link)  
 
 		print(links_in_posts)
 
 		time.sleep(2)
-subreddits_to_crawl = ['learnpython','java','Python']
+subreddits_to_crawl = ['learnpython']
 
 for sub in subreddits_to_crawl:
     post_finder(sub)
-
+comment_parser(post_links)
